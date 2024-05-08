c2498ac604b89aa423b22e792cc90974e79e2e0c
jakubslaby09 <jakubslaby09@gmail.com>
2024-05-08 07:31:13 +0200

    add vite config with an inline css plugin

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite"
+import { viteSingleFile } from "vite-plugin-singlefile"
+
+export default defineConfig({
+  plugins: [
+    viteSingleFile({
+      inlinePattern: [ "*.css" ],
+    }),
+  ],
+  server: {
+    port: 3200,
+    host: true,
+  },
+})
